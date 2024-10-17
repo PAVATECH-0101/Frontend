fb8d0c5337d8758f785cf33d5032793ddb849cb6
VincentAgunda <vincentagunda351@gmail.com>
2024-10-17 18:41:10 +0300

    access

---
+++ accessibility-app/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'; // Custom footer styling
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 SimplyAccees | Kenya</p>
+    </footer>
+  );
+};
+
+export default Footer;
