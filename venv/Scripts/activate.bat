0c0246fabba6fb84d379d5ad4a7251019fa7d90c
ARNOLD <achikiarnold@gmail.com>
2024-10-22 23:45:24 +0300

    updated the executable code

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Admin\Documents\hack4pwd-main\venv
+set VIRTUAL_ENV=C:\Users\Admin\Desktop\Frontend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
